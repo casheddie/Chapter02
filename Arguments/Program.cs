6465bb06c3d392e74541398cc5aedf4622a8f923
Eddie Reed <eddie.reedjr@gmail.com>
2024-07-11 07:29:06 -0400

    created Arguments project

---
+++ Arguments/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+WriteLine($"There are {args.Length} arguments.");
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify two coloros");
+    WriteLine("dotnet run red yellow 50");
+    return; // Stop Running
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the size of the cursor.");
+ 
+}
+
+    
--- Vocabulary/obj/Debug/net8.0/Vocabulary.AssemblyInfo.cs
+++ Vocabulary/obj/Debug/net8.0/Vocabulary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vocabulary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667087913a24f2a633cb25fe82a819d9b6b31a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4559c8b914de57502c5183dcb08d8ccbf52d94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vocabulary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vocabulary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
